50e2f90c470e5d21a6de992e5b73ede8679ed9f8
TrustDec <trusttheboy@gmail.com>
2021-12-15 23:46:14 +0800

    1.code

--- tools/src/App.js
+++ tools/src/App.js
@@ -1,10 +1,7 @@
-
-import TrustWallet from './utils/wallet.json'
 import React, { useState } from 'react'
-
-import './App.css'
-
-
+import TrustWallet from './utils/wallet.json'
+const inviterCode = "HGtfLi"
+const emailName = "trusttheboy"
 function getRandomAlphaNum(len) {
   var rdmString = "";
   for (; rdmString.length < len; rdmString += Math.random().toString(36).substr(2));
@@ -13,34 +10,28 @@ function getRandomAlphaNum(len) {
 function App() {
   const [cindex, setCindex] = useState(0)
   const request = async (addr) => {
-    const email = `trusttheboy+${getRandomAlphaNum(6)}@gmail.com`
+    const inviter = inviterCode
+    const email = `${emailName}+${getRandomAlphaNum(6)}@gmail.com`
     const url = "https://api.socialfi.ai/api/v1/register"
     let testRequest = new Request(url, {
       method: 'post',
       headers: {
         'Content-Type': 'application/json;charset=utf-8;'
       },
-      body: JSON.stringify(
-        {
-          "addr": addr,
-          "email": email,
-          "inviter": "HGtfLi"
-        }
-      )
+      body: JSON.stringify({ addr, email, inviter })
     })
     const response = await fetch(testRequest);
     let result = await response.json()
     console.log(`${cindex}:email:${email};addr:${addr}`, result)
     setCindex(cindex + 1)
   }
-  const getAddr = async (index) => {
+  const getAddr = () => {
     request(TrustWallet[cindex].address)
   }
   return (
     <div className="App">
-      <a href="javascript:void(0);" className="btn" onClick={getAddr}>搞{cindex}次</a>
+      <a href="javascript:void(0);" className="btn" onClick={getAddr}>Send Gmail:第{cindex}次</a>
     </div>
   );
 }
-
 export default App;
